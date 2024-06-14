625a1db4c1522ab792872ec51b807357dec11290
Aishwary Shah <49820149+aishwary11@users.noreply.github.com>
2024-06-14 12:36:39 +0530

    some more optimize

--- inoperator.ts
+++ inoperator.ts
@@ -1,35 +1,22 @@
+type Shape = Circle | Rectangle;
+
 type Circle = {
-  type: 'circle';
-  radius: number;
+  readonly radius: number;
 };
 
 type Rectangle = {
-  type: 'rectangle';
-  width: number;
-  height: number;
+  readonly width: number;
+  readonly height: number;
 };
-const calculateArea = (shape: Circle | Rectangle) => {
-  if ('radius' in shape) {
-    // shape is narrowed down to Circle
-    const area = Math.PI * shape.radius ** 2;
-    console.log('Area of the circle:', area);
-  } else {
-    // shape is narrowed down to Rectangle
-    const area = shape.width * shape.height;
-    console.log('Area of the rectangle:', area);
-  }
-};
-
 const circle: Circle = {
-  type: 'circle',
   radius: 5,
 };
 
 const rectangle: Rectangle = {
-  type: 'rectangle',
   width: 3,
   height: 4,
 };
+const calculateArea = (shape: Shape) => 'radius' in shape ? Math.PI * shape.radius ** 2 : shape.width * shape.height;
 
-calculateArea(circle); // Output: Area of the circle: 78.53981633974483
-calculateArea(rectangle); // Output: Area of the rectangle: 12
+console.log(calculateArea(circle)); // Output: Area of the circle: 78.53981633974483
+console.log(calculateArea(rectangle)); // Output: Area of the rectangle: 12
